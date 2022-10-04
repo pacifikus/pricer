d6e8008d209a151a205a4133a8ac6e18ee7df3ee
ssamyshkin <111680372+ssamyshkin@users.noreply.github.com>
2022-10-04 14:27:47 +0500

    Update CashFlow.py

--- Implementation/Products/CashFlow.py
+++ Implementation/Products/CashFlow.py
@@ -1,35 +1,58 @@
 from datetime import date
+from datetime import date
 from typing import List
+from abc import ABC, abstractmethod
 import pandas
 
-class CashFlow:
-    def __init__(
-        self,
-        Product: str #Product - VanillaStructuredProduct or Autocall
-    ):
-        self.Product = Product
+class CashFlow(ABC):
+    @abstractmethod
+    def init(self):
+        pass
 
-
+    @abstractmethod
     def getPaymentDates(self) -> List[date]:
-        return self.Product.getPaymentDates()
-
+        pass
+    
+    @abstractmethod
     def getPaymentAmount(
         self,
         paymentDate: [date],
         market: QuoteProvider
         ) -> float:
-        return self.Product.getPaymentAmount(self.getPaymentDates(), market)
+        pass
+
+class test_cash_flow_class(CashFlow):
+    def init(self):
+        pass
+
+    def getPaymentDates(self, Product) -> List[date]:
+        self.Product = Product
+        return self.Product.getPaymentDates()
+
+    def getPaymentAmount(
+        self,
+        Product,
+        paymentDate: [date],
+        market: QuoteProvider
+        ) -> float:
+        self.Product = Product
+        return self.Product.getPaymentAmount(self.getPaymentDates(Product), market)
     
     def getCashflow(
         self,
+        Product,
         currentDate: [date],
         paymentDate: [date],
         market: QuoteProvider
         ) -> pandas.Series:
-        
+        self.Product = Product
         flows = pandas.Series(self.getPaymentAmount(
-            self.getPaymentDates(),
+            Product,
+            self.getPaymentDates(Product),
             market), 
-            index=[self.getPaymentDates()], 
+            index=self.getPaymentDates(Product), 
             dtype=float)
+        print(flows.index, currentDate)
         return flows[flows.index <= currentDate]
+
+Flow = test_cash_flow_class()
