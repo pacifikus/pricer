157cff89c87dc49d61df6dc125b8eb5f53d5550a
pacificus <masterkristall@gmail.com>
2022-10-07 02:50:08 +0700

    Make pricer non-abstract, correct functions definitions

--- Implementation/Products/Pricer.py
+++ Implementation/Products/Pricer.py
@@ -1,50 +1,25 @@
-from abc import ABC, abstractmethod
 from datetime import date
 
 from Products.CashFlow import CashFlow
 
 
-class Pricer(ABC):
-    @abstractmethod
-    def init(self):
-        pass
-
-    @abstractmethod
-    def getValuationDate(self) -> date:
-        pass
-
-    @abstractmethod
-    def getDiscountFactor(self, paymentDate: date) -> date:
-        pass
-
-    @abstractmethod
-    def getCallOptionBasePrice(
-        self, underlying: str, strike: float, maturityDate: date
-    ) -> float:
-        pass
-
-    @abstractmethod
-    def CashFlowBasePrice(self, priceElement: CashFlow) -> float:
-        pass
-
-
-class class_for_test(Pricer):
-    def init(self):
+class Pricer:
+    def __init__(self):
         pass
 
     def getValuationDate(self) -> date:
         pass
 
-    def getDiscountFactor(self, paymentDate: date) -> date:
+    def getDiscountFactor(self, paymentDate: date) -> float:
         pass
 
     def getCallOptionBasePrice(
-        self, underlying: str, strike: float, maturityDate: date
+        self,
+        underlying: str,
+        strike: float,
+        maturityDate: date
     ) -> float:
         pass
 
-    def CashFlowBasePrice(self, priceElement: CashFlow) -> float:
+    def getCashFlowBasePrice(self, pricedElement: CashFlow) -> float:
         pass
-
-
-r = class_for_test()
