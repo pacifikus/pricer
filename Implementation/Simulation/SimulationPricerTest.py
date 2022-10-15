ca22888fa94f647b660b85b5322fb506dc5c10e4
pacificus <masterkristall@gmail.com>
2022-10-15 21:35:12 +0700

    Test refactoring

--- Implementation/Simulation/SimulationPricerTest.py
+++ Implementation/Simulation/SimulationPricerTest.py
@@ -1,27 +1,22 @@
+from collections import namedtuple
 from datetime import date
 from math import exp
 from typing import List
 from unittest import TestCase
+from unittest.mock import patch
+
+import numpy as np
 
 from Products.QuoteProvider import QuoteProvider
-from Simulation.CovarianceTermStructure import CovarianceTermStructure
-from Simulation.DiscountCurve import DiscountCurve
 from Simulation.SimulationPricer import SimulationPricer
-
-
-class CovarianceTermStructureStub(CovarianceTermStructure):
-    def getObservationDate(self) -> date:
-        pass
-
-    def getTotalCovariance(self, ForecastDate: date):
-        return 0.1
 
 
 class QuoteProviderStub(QuoteProvider):
     def __init__(self, value: float):
         self.__value = value
 
-    def getQuotes(self, ticker: str, observationDates: List[date]) -> List[float]:
+    def getQuotes(self, ticker: str, observationDates: List[date]) -> List[
+        float]:
 
         if ticker == "GAZP" and observationDates == [date(2022, 9, 1)]:
             return [self.__value]
@@ -29,26 +24,25 @@ class QuoteProviderStub(QuoteProvider):
             raise NotImplementedError()
 
 
-class DiscountCurveStub(DiscountCurve):
-    def getValuationDate(self) -> date:
-        return date(2022, 9, 1)
-
-    def getDiscountFactor(self, paymentDate) -> float:
-        rate = 0.1
-        if paymentDate == date(2022, 9, 1):
-            return exp(-rate * 30 / 365)
-        else:
-            raise NotImplementedError()
-
-
 class SimulationPricerTest(TestCase):
-    def setUp(self) -> None:
+    @patch('Simulation.CovarianceTermStructure')
+    @patch('Simulation.DiscountCurve')
+    def setUp(self, mockCovarianceTermStructure, mockDiscountCurve) -> None:
+        mockCovarianceTermStructure = mockCovarianceTermStructure
+        mockCovarianceTermStructure.getTotalCovariance.return_value = \
+            np.array([0.01])
+        rate = 0.1
+        self.__mockDiscountCurve = mockDiscountCurve
+        self.__mockDiscountCurve.getDiscountFactor.return_value = exp(
+            -rate * 30 / 365
+        )
+
         self.__testedPricer = SimulationPricer(
             underlyings=["GAZP"],
-            covariance=CovarianceTermStructureStub(),
+            covariance=mockCovarianceTermStructure,
             valuationDate=date(2022, 9, 1),
             originalMarket=QuoteProviderStub(250),
-            discountCurve=DiscountCurveStub(),
+            discountCurve=mockDiscountCurve,
         )
 
     def testValuationDate(self):
@@ -58,35 +52,24 @@ class SimulationPricerTest(TestCase):
         )
 
     def testDiscountFactor(self):
-        rate = 0.1
         date_ = date(2022, 9, 1)
         self.assertEqual(
-            self.__testedPricer.getDiscountFactor(date_), exp(-rate * 30 / 365)
+            self.__testedPricer.getDiscountFactor(date_),
+            self.__mockDiscountCurve.getDiscountFactor(date_)
         )
 
-    def testCallOptionBasePriceInTheMoneyPayoff(self):
-        result = self.__testedPricer.getCallOptionBasePrice(
-            underlying="GAZP",
-            strike=200,
-            maturityDate=date(2022, 10, 1),
-        )
-        expected = 51.71
-        self.assertEqual(result, expected)
-
-    def testCallOptionBasePriceOutOfTheMoneyPayoff(self):
-        result = self.__testedPricer.getCallOptionBasePrice(
-            underlying="GAZP",
-            strike=300,
-            maturityDate=date(2022, 10, 1),
-        )
-        expected = 0.45
-        self.assertEqual(expected, result)
-
-    def testCallOptionBasePriceAtTheMoneyPayoff(self):
-        result = self.__testedPricer.getCallOptionBasePrice(
-            underlying="GAZP",
-            strike=250,
-            maturityDate=date(2022, 10, 1),
-        )
-        expected = 10.96
-        self.assertEqual(expected, result)
+    def testCallOptionBasePrice(self):
+        TestConfig = namedtuple("TestConfig", "strike expected")
+        testMap = {
+            'InTheMoney': TestConfig(strike=200, expected=51.71),
+            'AtTheMoney': TestConfig(strike=250, expected=10.96),
+            'OutOfTheMoney': TestConfig(strike=300, expected=0.45),
+        }
+        for testCase, testConfig in testMap.items():
+            with self.subTest(name=testCase):
+                result = self.__testedPricer.getCallOptionBasePrice(
+                    underlying="GAZP",
+                    strike=testConfig.strike,
+                    maturityDate=date(2022, 10, 1),
+                )
+                self.assertAlmostEqual(result, testConfig.expected, places=1)
