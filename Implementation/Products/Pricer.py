06e864fff011f70cec4cb841b989d3082f0c8d61
pacificus <masterkristall@gmail.com>
2022-10-18 21:04:20 +0700

    Abstract Pricer

--- Implementation/Products/Pricer.py
+++ Implementation/Products/Pricer.py
@@ -1,18 +1,19 @@
 from datetime import date
+from abc import ABC, abstractmethod
 
 from Products.CashFlow import CashFlow
 
 
-class Pricer:
-    def __init__(self):
-        pass
-
+class Pricer(ABC):
+    @abstractmethod
     def getValuationDate(self) -> date:
         pass
 
+    @abstractmethod
     def getDiscountFactor(self, paymentDate: date) -> float:
         pass
 
+    @abstractmethod
     def getCallOptionBasePrice(
         self,
         underlying: str,
@@ -21,5 +22,6 @@ class Pricer:
     ) -> float:
         pass
 
+    @abstractmethod
     def getCashFlowBasePrice(self, pricedElement: CashFlow) -> float:
         pass
