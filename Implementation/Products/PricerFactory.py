76079dfc3f0f37332cd8ea27a70991bb48309dec
pacificus <masterkristall@gmail.com>
2022-10-20 19:04:07 +0700

    Fix typo

--- Implementation/Products/PricerFactory.py
+++ Implementation/Products/PricerFactory.py
@@ -12,6 +12,6 @@ class PricerFactory(ABC):
         self,
         valuationDate: date,
         underlyings: List[str],
-        marker: QuoteProvider
+        market: QuoteProvider
     ) -> Pricer:
         pass
