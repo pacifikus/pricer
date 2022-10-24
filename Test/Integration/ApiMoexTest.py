3174415f17ec39ada9c923a5a5691d1f064754ed
pacificus <masterkristall@gmail.com>
2022-10-24 18:50:04 +0700

    test simple get moex

--- Test/Integration/ApiMoexTest.py
+++ Test/Integration/ApiMoexTest.py
@@ -30,12 +30,10 @@ class ApiMoexTest(TestCase):
         # cls.__session.close()
 
     def testStockQuotesFeed(self):
-        res = self.__session.get(
-            'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/GAZP.json?'
-            'iss.json=extended&iss.meta=off&from=2022-10-09&till=2022-10-10&iss.only=history%2Chistory.cursor&'
-            'history.columns=TRADEDATE%2CCLOSE')
-
-        print(res.text)
+        res = apimoex.get_board_history(
+             self.__session,
+             'GAZP',
+        )
         # res = apimoex.get_board_history(
         #     self.__session,
         #     'GAZP',
